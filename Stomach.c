8757ccc221dd4cb07ae42b1df95f4e278915e4ee
Anthony Tsang <tsang.keep.art.in.stock@gmail.com>
2024-07-01 07:19:07 +0200

    repo init. Add a basic idea of the library

---
+++ Stomach.c
@@ -0,0 +1,115 @@
+#include "Stomach.h"
+#include <string.h>
+#include <assert.h>
+
+void* Stomach_Arena_fill(struct Stomach_Arena* arena, Stomach_u64 size)
+{
+  uint8_t*  fill_pointer = arena->fill_pointer;
+  uintptr_t alignment = 2 * sizeof(void*);
+  uintptr_t modulo = (uintptr_t) fill_pointer & (alignment - 1);
+  if (modulo != 0)
+  {
+    fill_pointer += alignment - modulo;
+  }
+  uint8_t*  fill_pointer_new = fill_pointer + size;
+  if (fill_pointer_new > ((uint8_t*)arena->fill_pointer + arena->capacity))
+  {
+    assert("Arena overflow");
+    fill_pointer = NULL;
+  }
+  else
+  {
+    arena->fill_pointer = fill_pointer_new;
+  }
+  return fill_pointer;
+}
+
+void  Stomach_Arena_split(struct Stomach_Arena* src, struct Stomach_Arena* dest, Stomach_u64 size)
+{
+  void* data = Stomach_Arena_fill(src, size);
+  dest->data = data;
+  dest->fill_pointer = data;
+  dest->capacity = size;
+}
+
+Stomach_Arena_Snapshot  Stomach_Arena_snapshot(struct Stomach_Arena* arena)
+{
+  return arena->fill_pointer;
+}
+
+void  Stomach_Arena_rollback(struct Stomach_Arena* arena, Stomach_Arena_Snapshot snapshot)
+{
+  arena->fill_pointer = snapshot;
+}
+
+void  Stomach_Arena_clear(struct Stomach_Arena* arena)
+{
+  arena->fill_pointer = arena->data;
+}
+
+void* Stomach_Array_push(Stomach_Array* array, Stomach_u64 item_size)
+{
+  uint8_t*  fill_pointer = array->fill_pointer;
+  uint8_t*  fill_pointer_new = fill_pointer + item_size;
+  if (fill_pointer_new > (fill_pointer + array->capacity))
+  {
+    assert("Arena overflow");
+    fill_pointer = NULL;
+  }
+  else
+  {
+    array->fill_pointer = fill_pointer_new;
+  }
+  return fill_pointer;
+}
+
+Stomach_u64  Stomach_Array_size(Stomach_Array* array, Stomach_u64 item_size)
+{
+  return ((uint8_t*)array->fill_pointer - (uint8_t*)array->data) / item_size;
+}
+
+void  Stomach_init(struct Stomach* stomach)
+{
+  stomach->arena_temporary.data = stomach->memory_temporary;
+  stomach->arena_temporary.fill_pointer = stomach->memory_temporary;
+  stomach->arena_temporary.capacity = STOMACH_TEMPORARY_SIZE;
+  stomach->lexer.array_input_string.data = stomach->input_string;
+  stomach->lexer.array_input_string.fill_pointer = stomach->input_string;
+  stomach->lexer.array_input_string.capacity = STOMACH_INPUT_STRING_SIZE;
+  stomach->lexer = (struct Stomach_Lexer) {0};
+  stomach->lexer.fd = -1;
+  stomach->lexer.arena_tokens.data = stomach->memory_tokens;
+  stomach->lexer.arena_tokens.fill_pointer = stomach->memory_tokens;
+  stomach->lexer.arena_tokens.capacity = STOMACH_TOKENS_SIZE;
+  Stomach_Arena_split(&stomach->lexer.arena_tokens, &stomach->lexer.tokens, sizeof(struct Stomach_Token*) * STOMACH_TOKENS_AMOUNT);
+  stomach->parser.arena_parse_tree.data = stomach->memory_parse_tree;
+  stomach->parser.arena_parse_tree.fill_pointer = stomach->memory_parse_tree;
+  stomach->parser.arena_parse_tree.capacity = STOMACH_PARSE_TREE_SIZE;
+}
+
+void  Stomach_reset(struct Stomach* stomach)
+{
+  Stomach_Arena_clear(&stomach->arena_temporary);
+  Stomach_Arena_clear(&stomach->lexer.array_input_string);
+  Stomach_Arena_clear(&stomach->lexer.arena_tokens);
+  Stomach_Arena_clear(&stomach->lexer.tokens);
+  stomach->lexer.fd = -1;
+  Stomach_Arena_clear(&stomach->parser.arena_parse_tree);
+}
+
+void  Stomach_Lexer_set_input_file(struct Stomach_Lexer* lexer, int fd)
+{
+  lexer->fd = fd;
+}
+
+void  Stomach_Lexer_set_input_string(struct Stomach_Lexer* lexer, struct Stomach_String input_string)
+{
+  char* input_string_dest = Stomach_Array_push(&lexer->array_input_string, sizeof(char)* input_string.length);
+  memcpy(input_string_dest, input_string.data, input_string.length); 
+}
+
+int  Stomach_lex(struct Stomach_Lexer* lexer, uint32_t amount_of_token)
+{
+  // struct Stomach_Lexer_Output output = Stomach_Lexer(input, 
+}
+
---
+++ Stomach.h
@@ -0,0 +1,144 @@
+#ifndef STOMACH_H
+#define STOMACH_H
+
+#include <stdint.h>
+
+#define KB  * (1024)
+#define MB  * (1048576)
+#define GB  * (uint64_t)(1073741824)
+
+#ifndef STOMACH_TEMPORARY_SIZE
+#define STOMACH_TEMPORARY_SIZE  (1 KB)
+#endif
+
+#ifndef STOMACH_INPUT_STRING_SIZE
+#define STOMACH_INPUT_STRING_SIZE (4 KB)
+#endif
+
+#ifndef STOMACH_TOKENS_SIZE
+#define STOMACH_TOKENS_SIZE (4 KB)
+#endif
+
+#ifndef STOMACH_TOKENS_AMOUNT
+#define STOMACH_TOKENS_AMOUNT 256
+#endif
+
+#ifndef STOMACH_PARSE_TREE_SIZE
+#define STOMACH_PARSE_TREE_SIZE (128 KB)
+#endif
+
+#define STOMACH_SIZE  (sizeof(struct Stomach))
+
+#ifndef NULL
+#define NULL (0)
+#endif
+
+typedef uint64_t  Stomach_u64;
+typedef void*     Stomach_Arena_Snapshot;
+struct Stomach_Arena
+{
+  void*       fill_pointer;
+  Stomach_u64 capacity;
+  void*       data;
+};
+
+void* Stomach_Arena_fill(struct Stomach_Arena* arena, Stomach_u64 size); // will align the item
+void  Stomach_Arena_split(struct Stomach_Arena* src, struct Stomach_Arena* dest, Stomach_u64 size);
+Stomach_Arena_Snapshot  Stomach_Arena_snapshot(struct Stomach_Arena* arena);
+void  Stomach_Arena_rollback(struct Stomach_Arena* arena, Stomach_Arena_Snapshot snapshot);
+void  Stomach_Arena_clear(struct Stomach_Arena* arena);
+
+typedef struct Stomach_Arena  Stomach_Array;
+
+void*         Stomach_Array_push(Stomach_Array* array, Stomach_u64 item_size); // should not need to align the item
+Stomach_u64   Stomach_Array_size(Stomach_Array* array, Stomach_u64 item_size);
+
+struct Stomach_Slice
+{
+  void*       data;
+  Stomach_u64 length;
+};
+
+struct Stomach_String
+{
+  char*       data;
+  Stomach_u64 length;
+};
+
+struct Stomach_Token;
+struct Stomach_Lexer_Output
+{
+  struct Stomach_Token* data;
+  Stomach_u64           length;
+};
+
+struct Stomach_Parse_Node;
+struct Stomach_Parser_Output
+{
+  struct Stomach_Parse_Node*  data;
+  Stomach_u64                 length;
+};
+
+int Stomach_Slice_is_valid(struct Stomach_Slice slice);
+
+struct Stomach_Token
+{
+  int                   type;
+  struct Stomach_String content;
+};
+
+struct Stomach_Lexer
+{
+  Stomach_Array         array_input_string;
+  struct Stomach_Arena  arena_tokens;
+  Stomach_Array         tokens; // data is Stomach_Token**
+  int                   start_of_array; // to make the tokens array a circular one
+  int                   fd;
+  struct Stomach_String input;
+};
+
+struct Stomach_Parse_Tree_Node
+{
+  int                   type;
+  struct Stomach_String content;
+
+  struct Stomach_Parse_Tree_Node* parent;
+  struct Stomach_Parse_Tree_Node* next;
+  struct Stomach_Parse_Tree_Node* prev;
+  struct Stomach_Parse_Tree_Node* first;
+  struct Stomach_Parse_Tree_Node* last;
+};
+
+struct Stomach_Parser
+{
+  struct Stomach_Arena            arena_parse_tree;
+  struct Stomach_Parse_Tree_Node* root;
+};
+
+// This is the memory layout of the whole parser
+struct Stomach
+{
+  struct Stomach_Arena  arena_temporary;
+  struct Stomach_Lexer  lexer;
+  struct Stomach_Parser parser;
+
+  uint8_t memory_temporary[STOMACH_TEMPORARY_SIZE];
+  char input_string[STOMACH_INPUT_STRING_SIZE];
+  union
+  {
+    struct Stomach_Token*  tokens[STOMACH_TOKENS_AMOUNT];
+    uint8_t memory_tokens[STOMACH_TOKENS_SIZE];
+  };
+  uint8_t memory_parse_tree[STOMACH_PARSE_TREE_SIZE];
+};
+
+extern struct Stomach_Lexer_Output  Stomach_Lexer(struct Stomach_String input, struct Stomach_Arena* arena);
+extern struct Stomach_Parser_Output Stomach_Parser(struct Stomach_Parser parser, struct Stomach_Lexer lexer);
+
+void  Stomach_init(struct Stomach* stomach);
+void  Stomach_reset(struct Stomach* stomach);
+void  Stomach_Lexer_set_input_file(struct Stomach_Lexer* lexer, int fd);
+void  Stomach_Lexer_set_input_string(struct Stomach_Lexer* lexer, struct Stomach_String input_string);
+int   Stomach_lex(struct Stomach_Lexer* lexer, uint32_t amount_of_token);
+
+#endif
